4ab302d12b4d91eed835750488b6864fb3df99b9
ChauBui00 <c.bui3@share.epsb.ca>
2022-11-11 18:37:32 -0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 document.getElementById("img").addEventListener("click", random);
+let outputEl = get.getElementById("output").value;
+let input = getElementById("input").value;
+
+function random(){
+    if (input=== hey) {
+        outputEl.innerHTML = "Pleae ask question!";
+    } else {
+        outputEl.innerHTML = "Have a nice day!";
+    } 
+}
