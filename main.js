313d075e54af191780fd97f76dce45b59f8f2fdd
ChauBui00 <c.bui3@share.epsb.ca>
2022-11-13 00:08:09 -0500

    update

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 
 let imgEl = document.getElementById("img");
-let outputEl = get.getElementById("out-put");
-let inputEl = getElementById("input");
+let outputEl = document.getElementById("out-put");
+let inputEl = document.getElementById("input");
+
 
 imgEl.addEventListener("click", random);
 
 function random(){
-    if (inputEl === hey) {
-        outputEl.innerHTML = "Pleae ask question!";
-    } else {
-        outputEl.innerHTML = "Have a nice day!";
-    } 
+
+let randNum = Math.random();
+
+if (randNum < 0.1) {
+    outputEl.innerHTML = "No.";
+} else if (randNum < 0.2){
+    outputEl.innerHTML = "May be.";
+}else if (randNum < 0.3) {
+    outputEl.innerHTML = "As it see it, yes.";
+}else if (randNum < 0.4){
+    outputEl.innerHTML = "Next question."
+}else if (randNum < 0.5){
+    outputEl.innerHTML ="Concentrate and ask again bruh."
+}else if (randNum < 0.6){
+    outputEl.innerHTML ="Outlook not so good, smh."
+}else if (randNum < 0.7){
+    outputEl.innerHTML ="NO WAY."
+}else if (randNum < 0.8){
+    outputEl.innerHTML ="You drop your cap."
+}else if (randNum < 0.9){
+    outputEl.innerHTML ="Good one!"
+}
+else {
+    outputEl.innerHTML = "Slayyyy Bestie!!!!!";
+}
 }
